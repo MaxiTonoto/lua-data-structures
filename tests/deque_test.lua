4e480a85c2d215c92c4df52f6fb0a30e292d9d30
MaxiTonoto <supursandbox@gmail.com>
2025-11-16 19:37:01 -0300

    Add deque tests

--- tests/deque_test.lua
+++ tests/deque_test.lua
@@ -4,20 +4,90 @@ local Deque = require("deque")  -- import Deque
 
 function main()
     local my_deque = Deque:new()
-
-    for i = 1, 11 do
-        my_deque:add_last(i)
-    end
-    for k, v in ipairs(my_deque._data) do
-        print(k, v)
-    end
-    print(); print(); print();
-    for i = 1, 11 do
+    print("Created deque: my_deque")
+    print("my_deque:is_empty()=" .. tostring(my_deque:is_empty()))
+    print("String representation:" .. tostring(my_deque))
+    print("\nAdding first: 1 to 5")
+    for i = 1, 5 do
         my_deque:add_first(i)
     end
-    for k, v in ipairs(my_deque._data) do
-        print(k, v)
+    print("my_deque=" .. tostring(my_deque))
+    print("#my_deque=" .. tostring(#my_deque))
+    print("my_deque:is_empty()=" .. tostring(my_deque:is_empty()))
+    print("\nAdding last: 'a' to 'e'")
+    for i = 97, 97+4 do
+        my_deque:add_last(string.char(i))
     end
+    print("my_deque=" .. tostring(my_deque))
+
+    print("\nDeleting first: 3 times")
+    for i = 1, 3 do
+        print("DELETED: " .. tostring(my_deque:delete_first(i)))
+    end
+    print("my_deque=" .. tostring(my_deque))
+
+    print("\nDeleting last: 3 times")
+    for i = 1, 3 do
+        print("DELETED: " .. tostring(my_deque:delete_last(i)))
+    end
+    print("my_deque=" .. tostring(my_deque))
+
+    print("\nAdd_first 500,000 items test")
+    local new_deque = Deque:new()
+    local add_first_500k_items = Tests.time_test(
+        function()
+           for i = 1, 500000 do
+                new_deque:add_first(i)
+            end 
+        end
+    )
+    print("Elapsed time: " .. add_first_500k_items .. " seconds")
+
+    print("\nAdd_last 500,000 items test")
+    local add_last_500k_items = Tests.time_test(
+        function()
+           for i = 1, 500000 do
+                new_deque:add_last(i)
+            end 
+        end
+    )
+    print("Elapsed time: " .. add_last_500k_items .. " seconds")
+
+    print("\nDelete_first 500,000 items test")
+    local delete_first_500k_items = Tests.time_test(
+        function()
+           for i = 1, 500000 do
+                new_deque:delete_first(i)
+            end 
+        end
+    )
+    print("Elapsed time: " .. delete_first_500k_items .. " seconds")
+
+    print("\nDelete_last 500,000 items test")
+    local delete_last_500k_items = Tests.time_test(
+        function()
+            for i = 1, 500000 do
+                new_deque:delete_last(i)
+            end 
+        end
+    )
+    print("Elapsed time: " .. delete_last_500k_items .. " seconds")
+
+    print("\nTotal time to add 1,000,000 items: " .. 
+        add_first_500k_items + add_last_500k_items
+        .. " seconds"
+    )
+    print("Total time to delete 1,000,000 items: " .. 
+        delete_first_500k_items + delete_last_500k_items
+        .. " seconds"
+    )
+    print("Total time: "
+        .. add_first_500k_items
+        + add_last_500k_items
+        + delete_first_500k_items
+        + delete_last_500k_items
+        .. " seconds"
+    )
 end
 
 if ... == nil then
