eb8cdb6b9452e731b36bba72557f651ece42221b
MaxiTonoto <supursandbox@gmail.com>
2025-11-15 22:18:36 -0300

    Add time tests

--- tests/queue_test.lua
+++ tests/queue_test.lua
@@ -27,6 +27,28 @@ function main()
     print("my_queue:dequeue()=" .. my_queue:dequeue())
     print("my_queue=" .. tostring(my_queue))
     print("#my_queue=" .. #my_queue)
+
+    print("\nQueue 1,000,000 items test:")
+    local start_time1 = os.clock()
+    local new_queue1 = Queue:new()
+    for i = 1, 1000000 do
+        new_queue1:enqueue(i)
+    end
+    local end_time1 = os.clock()
+    local elapsed_time1 = end_time1 - start_time1
+    print("Elapsed time: " .. elapsed_time1 .. " seconds")
+
+    print("\nDequeue 1,000,000 items test:")
+    local start_time2 = os.clock()
+    for i = 1, 1000000 do
+        new_queue1:dequeue(i)
+    end
+    local end_time2 = os.clock()
+    local elapsed_time2 = end_time2 - start_time2
+    print("Elapsed time: " .. elapsed_time2 .. " seconds")
+
+    print("\nTotal time to enqueue and dequeue 1,000,000 items: "
+            .. elapsed_time1 + elapsed_time2 .. " seconds")
 end
 
 if ... == nil then
--- tests/stack_test.lua
+++ tests/stack_test.lua
@@ -19,6 +19,28 @@ function main()
     my_stack:clear()
     print("tostring(my_stack)=" .. tostring(my_stack))
     print("my_stack:is_empty()=" .. tostring(my_stack:is_empty()))
+
+    print("\nPush 1,000,000 items test:")
+    local start_time1 = os.clock()
+    local new_stack = Stack:new()
+    for i = 1, 1000000 do
+        new_stack:push(i)
+    end
+    local end_time1 = os.clock()
+    local elapsed_time1 = end_time1 - start_time1
+    print("Elapsed time: " .. elapsed_time1 .. " seconds")
+
+    print("\nPop 1,000,000 items test:")
+    local start_time2 = os.clock()
+    for i = 1, 1000000 do
+        new_stack:pop()
+    end
+    local end_time2 = os.clock()
+    local elapsed_time2 = end_time2 - start_time2
+    print("Elapsed time: " .. elapsed_time2 .. " seconds")
+
+    print("\nTotal time to push and pop 1,000,000 items: "
+            .. elapsed_time1 + elapsed_time2 .. " seconds")
 end
 
 -- this only executes if the current module was executed
